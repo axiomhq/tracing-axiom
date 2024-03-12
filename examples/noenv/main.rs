5bd461d84c0b7a77159d4868ce2290b291c5a4f9
Heinz N. Gies <heinz@licenser.net>
2024-03-12 15:50:18 +0100

    Update examples/noenv/main.rs

--- examples/noenv/main.rs
+++ examples/noenv/main.rs
@@ -12,7 +12,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .with_tags(&[("aws_region", "us-east-1")]) // Set otel tags
         .with_dataset("tracing-axiom-examples") // Set dataset
         .with_token("xaat-some-valid-token") // Set API token
-        .with_url("http://localhost:4318") // Set URL, local Jaeger can be changed to any OTEL endpoint
+        .with_url("http://localhost:4318") // Set URL, can be changed to any OTEL endpoint
         .init()?; // Initialize tracing
 
     let uuid = Uuid::new_v4();
